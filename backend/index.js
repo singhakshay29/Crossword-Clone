a8f4f7b73065c482d84a0241f81ae6bb96282181
akshaysingh29 <akshay.singh@uniproworld.com>
2024-09-23 19:54:07 +0530

    vercel updates

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,19 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const dotenv = require("dotenv").config();
-const connectDB=require('./config/dbconnection');
+const connectDB = require("./config/dbconnection");
 const bookRoutes = require("./routes/routes");
-const userRoutes = require('./routes/authroutes');
-
-
+const userRoutes = require("./routes/authroutes");
 
 connectDB();
-const app=express();
+const app = express();
 const port = process.env.PORT || 8080;
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-app.use('/user',userRoutes);
-app.use('/book',bookRoutes);
+app.use("/user", userRoutes);
+app.use("/book", bookRoutes);
 
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-  
+module.exports = app;
