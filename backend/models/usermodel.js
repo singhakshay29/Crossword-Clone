77ad91925241e737689950f3d79ac76cb1382bd7
akshaysingh29 <akshay.singh@uniproworld.com>
2024-09-23 18:54:58 +0530

    usermodel update

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,29 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const userSchema=mongoose.Schema({
-    first:{
-        type:String,
-        required:true
+const userSchema = mongoose.Schema(
+  {
+    first: {
+      type: String,
+      required: true
     },
-    last:{
-        type:String,
-        required:true
+    last: {
+      type: String,
+      required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true
+    },
+    wishlist: {
+      type: Array,
+      default: []
+    },
+    cart: {
+      type: Array,
+      default: []
     }
-},
-    {
-        timeStamps:true
-    }
-    
-)
+  },
+  {
+    timeStamps: true
+  }
+);
 
-module.exports=mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
