b738b312c2136769d6d61cc3c42b0c42d96901f5
akshaysingh29 <akshay.singh@uniproworld.com>
2024-09-27 13:44:15 +0530

    files updates

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { MdArrowForwardIos } from "react-icons/md";
 import { MdArrowBackIos } from "react-icons/md";
 
-
-export default function Carousel() {
-    const [currentIndex, setCurrentIndex] = useState(0);
+export default function CarouselSection() {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const slides = [
-    { id: 1, url:'https://www.crossword.in/cdn/shop/files/sudha_murty_banner_1296x@2x.png?v=1724154809' },
-    { id: 2, url:'https://www.crossword.in/cdn/shop/files/Preorder_banner_july_1296x@2x.jpg?v=1720598258' },
-    { id: 3,url:'https://www.crossword.in/cdn/shop/files/Tweak_India_Teen_Couple_Have_Fun_Outdoors_Banners_Desktop_copy_1296x@2x.jpg?v=1722490431', },
-    { id: 4,url:'https://www.crossword.in/cdn/shop/files/Half_Price_banners_50_off-05_1_1296x@2x.jpg?v=1720412306', },
-    {id: 5,url:'https://www.crossword.in/cdn/shop/files/Fiction_Addiction_Banner_Desktop_copy_1296x@2x.jpg?v=1716985828'}
+    {
+      id: 1,
+      url: "https://www.crossword.in/cdn/shop/files/sudha_murty_banner_1296x@2x.png?v=1724154809",
+    },
+    {
+      id: 2,
+      url: "https://www.crossword.in/cdn/shop/files/Preorder_banner_july_1296x@2x.jpg?v=1720598258",
+    },
+    {
+      id: 3,
+      url: "https://www.crossword.in/cdn/shop/files/Tweak_India_Teen_Couple_Have_Fun_Outdoors_Banners_Desktop_copy_1296x@2x.jpg?v=1722490431",
+    },
+    {
+      id: 4,
+      url: "https://www.crossword.in/cdn/shop/files/Half_Price_banners_50_off-05_1_1296x@2x.jpg?v=1720412306",
+    },
+    {
+      id: 5,
+      url: "https://www.crossword.in/cdn/shop/files/Fiction_Addiction_Banner_Desktop_copy_1296x@2x.jpg?v=1716985828",
+    },
   ];
 
   const handleNext = () => {
@@ -26,7 +40,7 @@ export default function Carousel() {
   };
   return (
     <div className="h-80 mt-52 w-full overflow-hidden">
-        <div
+      <div
         className="flex transition-transform duration-300 ease-in-out"
         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
       >
@@ -35,7 +49,7 @@ export default function Carousel() {
             key={slide.id}
             className={`min-w-full mt-12 h-64 flex items-center justify-center text-white`}
           >
-            <img  src={slide.url}/>
+            <img src={slide.url} />
           </div>
         ))}
       </div>
@@ -43,14 +57,14 @@ export default function Carousel() {
         onClick={handlePrev}
         className="absolute top-1/2 left-4 transform -translate-y-1/2  p-2"
       >
-    <MdArrowBackIos className=" text-3xl"  />
+        <MdArrowBackIos className=" text-3xl" />
       </button>
       <button
         onClick={handleNext}
         className="absolute top-1/2 right-4 transform -translate-y-1/2 p-2 "
       >
-        <MdArrowForwardIos className=" text-3xl"  />
+        <MdArrowForwardIos className=" text-3xl" />
       </button>
     </div>
-  )
+  );
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,16 @@
-import {Link} from "react-router-dom";
-import Navbar from "../components/Navbar";
+import {Link, useNavigate} from "react-router-dom";
 import { useState} from "react";
 import axios from "axios";
+import RotateLoader from "react-spinners/RotateLoader";
 
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 
 export default function SignUp() {
+  const [loading, setLoading] = useState(false);
   const [userDetails, setUserDetails] = useState({
     first:null,
     last:null,
@@ -12,6 +18,8 @@ export default function SignUp() {
     password:null,
   });
   const[error,setError]=useState(null);
+  const navigate = useNavigate();
+  
   function inputDetails(e){
     setError(null);
     setUserDetails((prev)=> ({
@@ -25,7 +33,6 @@ export default function SignUp() {
       e.preventDefault();
     }
     if (!userDetails.first || !userDetails.password || !userDetails.email) {
-      console.log(userDetails);
       setError("All Fields must be filled");
     } else if (!userDetails.email.includes("@")) {
       setError("Email is invalid");
@@ -36,20 +43,36 @@ export default function SignUp() {
   
   function submittingDetails(){
     const {first,last,email,password}=userDetails;
-    console.log(first,last,email,password);
+    setLoading(true); 
       axios.post('http://localhost:8081/user/signup',{
         first,last,email,password
       }).then(response => {
         console.log("Details submitted successfully:", response.data);
+        navigate('/'); 
       })
       .catch(error => {
         console.error("There was an error submitting the details:", error);
+        setError("Something went Wrong")
+      })
+      .finally(() => {
+        setLoading(false); 
       });
   }
     
   return (
     <>
-    <Navbar/>
+    {loading ? (
+        <div className="flex items-center justify-center border-2 border-solid  h-lvh  mt-8">
+          <RotateLoader
+            color={"#FFE300"}
+            loading={loading}
+            speedMultiplier={1}
+            cssOverride={override}
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>):(
     <div className="flex">
         <div className="mt-56 w-1/2 ml-4">
             <img width="600px" height="300px" src="https://cdn.shopify.com/s/files/1/0648/3066/9017/files/SignIn_d042cc21-da30-4fa8-83ea-fbaac6e548e9.jpg?v=1689674498"/>
@@ -76,6 +99,7 @@ export default function SignUp() {
            
         </div>
     </div>
+        )}
     </>
   )
 }
