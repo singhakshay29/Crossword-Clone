5bc5d7ddb5406a9632de427642fd91d410d51161
akshaysingh29 <akshay.singh@uniproworld.com>
2024-10-01 14:19:27 +0530

    cart model and routes added and updates

--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const validateToken = require('../middleware/validateToken')
-const { registerUser, loginUser, currentUser, wishlist_Add,getwishlist,wishlist_Del } =require('../controller/Auth');
+const { registerUser, loginUser, currentUser, addToWishlist,getWishlist,removeWhislistItem, addToCart, getCart, removeCartItem, updateCartItem } =require('../controller/Auth');
 
 router.post('/signin',loginUser);
 router.post('/signup',registerUser);
 router.get("/current", validateToken, currentUser);
-router.post('/wishlist',validateToken, wishlist_Add);
-router.get('/wishlist',validateToken,getwishlist);
-router.delete('/wishlist/:productId',validateToken,wishlist_Del)
+
+router.get('/wishlist',validateToken,getWishlist);
+router.post('/wishlist/add',validateToken, addToWishlist);
+router.delete('/wishlist/remove/:productId',validateToken,removeWhislistItem);
+
+router.get('/cart',validateToken,getCart);
+router.post('/cart/add',validateToken, addToCart);
+router.put('/cart/update',validateToken,updateCartItem);
+router.delete('/cart/remove/:productId',validateToken,removeCartItem);
 
 
 module.exports=router;
